f40bae46b1fc8ac0e0a602e2f758aff1b4d4b5ba
thanhthai2604 <thanhthai2604@gmail.com>
2023-01-12 16:04:09 +0700

    Task_Time_Keeping_Management_ThaiNT_FontEnd

---
+++ src/app/shared/services/hr-document.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {BaseService} from '@core/base.service';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '@env/environment';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HrDocumentService extends BaseService {
+
+  constructor(public http: HttpClient) {
+    super(http, `${environment.apiUrl}/document`);
+  }
+  getAllDoc(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/document`);
+  }
+  createDocument(formData: FormData): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/document/create`, formData);
+  }
+  updateDocument(formData: FormData): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/document/update`, formData);
+  }
+
+
+}
